44ad4b3a9ce1b9c0966f7b5dc6d73f7f1ac2eb6b
Joshua <ezejoshuac@gmail.com>
2022-10-26 14:20:07 +0100

    Implement object oriented programming
    - Create class for books
    - Create class to render books
    - Create methods to add, remove and display books

--- main.js
+++ main.js
@@ -1,51 +1,64 @@
 function addToLocalStorage(data) {
-  return localStorage.setItem('books', JSON.stringify(data));
+  return localStorage.setItem("books", JSON.stringify(data));
 }
 
 function getFromLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
 }
 
-let collections = [];
-
-function addBookToCollection(title, author) {
-  const id = Math.random();
-  collections.push({ title, author, id });
-  addToLocalStorage(collections);
-  return collections;
+class Book {
+  constructor(title, author, id = Math.random()) {
+    this.title = title;
+    this.author = author;
+    this.id = id;
+  }
 }
 
-function displayBooks() {
-  const getLocalData = getFromLocalStorage('books');
-  collections = getLocalData;
-  const bookSection = document.querySelector('.books-section');
-  bookSection.replaceChildren();
-  collections.forEach((book) => {
-    const singleBook = document.createElement('div');
-    singleBook.innerHTML = `<div>${book.title}</div>
+class Render {
+  collections = [];
+
+  static addBookToCollection(title, author) {
+    const book = new Book(title, author);
+    this.collections = this.collections.concat(book);
+    console.log(book);
+    addToLocalStorage(this.collections);
+    return this.collections;
+  }
+
+  static displayBooks() {
+    const getLocalData = getFromLocalStorage("books");
+    this.collections = getLocalData;
+    const bookSection = document.querySelector(".books-section");
+    bookSection.replaceChildren();
+    this.collections.forEach((book) => {
+      const singleBook = document.createElement("div");
+      singleBook.innerHTML = `<div>${book.title}</div>
         <div>${book.author}</div>
         <button data-id=${book.id} class='remove'>Remove</button>
         <hr>`;
-    bookSection.appendChild(singleBook);
-  });
-  /* eslint-disable no-use-before-define */
-  removeBookFromCollection();
-}
-function removeBookFromCollection() {
-  const removeBtn = document.querySelectorAll('.remove');
-  removeBtn.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-      const id = e.target.getAttribute('data-id');
-      collections = collections.filter((el) => el.id !== Number(id));
-      addToLocalStorage(collections);
-      displayBooks();
+      bookSection.appendChild(singleBook);
     });
-  });
+    Render.removeBookFromCollection();
+  }
+
+  static removeBookFromCollection() {
+    const removeBtn = document.querySelectorAll(".remove");
+    removeBtn.forEach((btn) => {
+      btn.addEventListener("click", (e) => {
+        const id = e.target.getAttribute("data-id");
+        this.collections = this.collections.filter(
+          (el) => el.id !== Number(id)
+        );
+        addToLocalStorage(this.collections);
+        Render.displayBooks();
+      });
+    });
+  }
 }
 
-const form = document.getElementById('book-form');
+const form = document.getElementById("book-form");
 
-form.addEventListener('submit', (e) => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const title = form.elements.title.value;
@@ -53,13 +66,13 @@ form.addEventListener('submit', (e) => {
   if (!title || !author) {
     return;
   }
+  Render.addBookToCollection(title, author);
+  // addBookToCollection(title, author);
 
-  addBookToCollection(title, author);
-
-  displayBooks();
+  Render.displayBooks();
   form.reset();
 });
 
-if (localStorage.getItem('books')) {
-  displayBooks();
+if (localStorage.getItem("books")) {
+  Render.displayBooks();
 }
